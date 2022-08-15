36c1e4bdc2b4b5e0c2feaceec8b7c378ddf63ac9
khaled_nadam <khalednadam@gmail.com>
2022-08-15 18:14:23 +0300

    fix backend

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ const PORT = process.env.PORT || 8000;
 const express = require('express');
 require('dotenv').config();
 const fetch = require("node-fetch");
-
 const app = express();
-
 const cors = require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions));
+app.use(cors());
 
-app.get('/', async (req, res) =>{
+app.get('/', (req, res) =>{
+    res.json('Hi')
+});
+app.get('/:countryCode', async (req, res) =>{
     try{
-        const response = await fetch(`https://calendarific.com/api/v2/holidays?&api_key=${process.env.REACT_APP_CALENDARIFIC_API_KEY}&country=PK&year=2022`);
+        const response = await fetch(`https://calendarific.com/api/v2/holidays?&api_key=${process.env.REACT_APP_CALENDARIFIC_API_KEY}&country=${req.params.countryCode}&year=2022`);
         if(!response.ok){
             throw new Error(`Error: ${response.status}`);
         }
