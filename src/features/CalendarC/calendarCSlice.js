4f028215aac342b497b717a03aec7c1bc575ba68
khaled_nadam <khalednadam@gmail.com>
2022-08-15 20:10:40 +0300

    local

--- src/features/CalendarC/calendarCSlice.js
+++ src/features/CalendarC/calendarCSlice.js
@@ -5,7 +5,7 @@ export const loadEvents = createAsyncThunk(
     'calendarC/loadEvents',
     async (countryCode) =>{
         try{
-            const response = await fetch(`https://holiday-calendar-app.herokuapp.com/${countryCode}`);
+            const response = await fetch(`http://localhost:8000/${countryCode}`);
             if(!response.ok){
                 throw new Error(`Error: ${response.status}`);
             }
